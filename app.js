35d6ca48dce82c34e592b7d81dc00601910fdbcf
Isuru Sahan <90211418+IT20133290@users.noreply.github.com>
2022-04-16 14:43:16 +0530

    remove test

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const app = express();
 
-
-// app.get('/', async (req, res) => {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.write('Hello World!');
-//     res.end();
-// })
-
 app.use(require('./test'))
 
 app.listen(8090, () => {
